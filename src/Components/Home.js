9c1530a5d3b5839773bb03a26efee5bb9b46ad49
Vishesh Jawa <61270409+jawachipcookie@users.noreply.github.com>
2023-04-15 01:48:53 +0530

    new commit

---
+++ src/Components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../Styles/Home.css'
+import dashboard_image from '../Assets/dashboard_image.png.png'
+
+function Home() {
+    return (
+        <div className='home_main_container'>
+            <div className='home_button'>
+                <p>Just Launched Ghost 3.0</p>
+            </div>
+            <div className='home_heading'>
+                <p>Turn your audience <br /> <span className='business'>into a business</span></p>
+            </div>
+            <div className='home_background'></div>
+            <img className='home_image' src={dashboard_image} />
+        </div>
+    )
+}
+
+export default Home
